14593e629708e206a97331ccd8b2db3af0c8f62f
Koushike-kun <koushikkalyani369@gmail.com>
2023-07-05 16:45:08 +0530

    ARM work

---
+++ ARM/flash.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo python3 /home/koushike-kun/TinyFPGA-Programmer-Application/tinyfpga-programmer-gui.py --port /dev/ttyACM0  --appfpga top.bin --m4app $1 --mode m4-fpga --reset
+
+
+
