23d6be0d5cf5c30cba4664341eccc6f26d53768e
Koushike-kun <koushikkalyani369@gmail.com>
2023-07-12 16:14:05 +0530

    Vectors

--- Vectors/code/straightline.py
+++ Vectors/code/straightline.py
@@ -27,7 +27,7 @@ print(f"y = {y_solution}")
 def line3(x):
   return (-1*x +(x_solution + y_solution))
 # Define the x values for the lines
-x_values = np.linspace(-10, 10, 100)
+x_values = np.linspace(-0.7, 0.7, 1000)
 
 # Calculate the y values for each line
 y_values1 = line1(x_values)
@@ -43,26 +43,19 @@ plt.plot(x_values, y_values2, label='2x - 3y + 1 = 0')
 #plot solution line i.e line3
 plt.plot(x_values, y_values3, label='x + y = 6/13')
 
+# Plot the intersection point
+plt.plot(x_solution, y_solution, 'ro', label='Intersection Point')
+
 # Plot x and y axes
 plt.axhline(0, color='black', linewidth=0.5)
 plt.axvline(0, color='black', linewidth=0.5)
 
 # Set the limits of the plot
-plt.xlim(-10, 10)
-plt.ylim(-10, 10)
+def display_coordinates(x, y):
+    plt.scatter(x, y, color='green')  # Plot the point
+    plt.text(x, y, f'({x}, {y})', verticalalignment='bottom')
+display_coordinates((x_solution + y_solution), 0)
+display_coordinates(0,(x_solution + y_solution))
 
-# Add labels and title
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Line Passing Through Intersection with Equal Intercepts')
-
-# Plot the intersection point
-plt.plot(x_intersect, y_intersect, 'ro', label='Intersection Point')
-
-# Add legend
+plt.xlim(-0.7, 0.7)
 plt.legend()
-
-# Show the plot
-plt.grid(True)
-plt.show()
-
